86b1361a70b68fcd32237257d840086361e2e06a
Vylich <ilya.sidorenko.99@gmail.com>
2023-09-20 15:28:02 +0300

    .

--- source/js/map.js
+++ source/js/map.js
@@ -12,7 +12,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '/img/map/map-pin.svg',
+  iconUrl: '../img/map/map-pin.svg',
   iconSize: [38, 50],
   iconAnchor: [19, 50],
 });
